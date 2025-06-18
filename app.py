5d90ca3c14504d3c0e261e8d82c9e5a5305e199e
Barrett Sapunjis <barrettsapunjis@me.com>
2025-06-18 13:11:18 -0600

    Updated GUI and added functions to return movies based on actor and return actor based on movies

--- app.py
+++ app.py
@@ -8,18 +8,27 @@ def interactiveCLI():
     while(True):
         userIn = int(input("Please select an option for what you would like to do \n" \
                         "[1] find movies by actor \n" \
-                        "[2] find actors in movie \n" \
+                        "[2] find actors by movie \n" \
                         "[3] find movies by date \n"))
         
         match userIn:
             case 1:
                 newIn = input(f"You have selected option [{userIn}]: find movies by actor \n" \
                             "Please enter the name of the actor you would like to find or enter 0 for the main menu: ")
+                if(newIn == "0"):            continue
+
+                else:
+                    print(dh.findMoviesByActor(data, newIn))
+
+            case 2:
+                newIn = input(f"You have selected option [{userIn}]: find actors by movie \n" \
+                            "Please enter the name of the movie you would like to see data on, or enter 0 for the main menu: ")
                 if(newIn == "0"):
                     continue
 
                 else:
-                    print(dh.findActorMovies(data, newIn))
+                    print(dh.findActorsByMovie(data, newIn))
+                    
 
 
 
--- dataHandler.py
+++ dataHandler.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 def constructData():
     actors = pd.read_excel("testData/testActors.xlsx")
     movies = pd.read_excel("testData/testMovies.xlsx")
@@ -9,11 +10,31 @@ def constructData():
 
     merge2 = pd.merge(merge1, ratings, on="movie", how="left")
 
+    merge2['movie'] = merge2['movie'].astype(str)
+
     return merge2
 
-def findActorMovies(data, actor):
+def findMoviesByActor(data, actor):
     print(f"selected actor {actor}")
-    viewingData = data.groupby("actors")["movie"]
-    grouped = pd.DataFrame(viewingData)
-    print(grouped)
-    return grouped[f"{actor}"][:]
+    viewingData = data[['actors', 'movie', 'title']].groupby("actors")
+    return viewingData.get_group(actor).reset_index(drop=True)
+
+def findActorsByMovie(data, movie):
+
+    viewingData = data[['title', 'movie', 'actors']].groupby("title")
+
+    df = data.sort_values(by=["title", "movie"]).reset_index(drop=True)
+
+
+    viewingData = viewingData.get_group(movie).set_index(['title', 'movie', 'actors'])
+    
+    df = df[df['title'] == movie]
+        # Blank out duplicate 'title' and 'movie' except for first occurrence
+    df.loc[1:, 'title'] = df['title'].mask(df['title'] == df['title'].shift()).fillna('')
+    df.loc[1:, 'movie'] = df['movie'].mask(df['movie'] == df['movie'].shift()).fillna('')
+
+    return df.reset_index(drop=True)
+
+def getActorStats(data, actor):
+    pass 
+
