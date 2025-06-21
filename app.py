59acd72dde19db8108c7373862dfc51ee193493c
mcheema88 <marley.cheema1@ucalgary.ca>
2025-06-21 15:39:04 -0600

    Okay I like this logic -> esse ntially works I want (I think) now time to debug and add my corresponding functions in dataHandler

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def interactiveCLI():
                         "Please choose from the following options and enter the number of your choise"   
                         "[1] Get movies by genre. \n" \
                         "[2] Get movies by release date. \n" \
-                        "[3] Get movies by actors. \n"\
+                        "[3] Get movies by actors/actresses. \n"\
                         "[4] Get movies by ratings \n"
                         " Please enter a number followed by enter \n"))
         
@@ -51,13 +51,33 @@ def interactiveCLI():
                     case 2: 
                         newNewIn = input(f"You have selected option [{newIn}]: Get movies before a certain year. \n" \
                             "Please enter the year that you like to only see movies released after that date (not including)")
-                        year1 = newNewIn
-                        year2 = 0
+                        year1 = 0
+                        year2 = newNewIn
                         
-                        currentData = dh.getMoviesForReleaseDate(currentData, optionYears, year1, year2)
+                        currentData = dh.getMoviesForReleaseDate(currentData, year1, year2)
 
+                    case 3: 
+                        year1 = input(f"You have selected option [{newIn}]: Get movies between certain years. \n" \
+                            "Please enter the year you would like for your lower limit (not including) ")
+                        year2 = input( "Please enter the year you would like for your upper limit (not including) ")
+                        currentData = dh.getMoviesForReleaseDate(currentData, year1, year2)
 
+            case 3:
+                newIn = input(f"You have selected option [{userIn}]: Get movies by Actors/Actresses. \n" \
+                            "Please enter the name of actor/actress of your choice")
+                actorActress = newIn
+                currentData = dh.getMoviesForActorActress(currentData, actorActress)
+            
+            case 4:
+                newIn = input(f"You have selected option [{userIn}]: Get movies by Rating. \n" \
+                            "Please enter the minimum rating threshold you would like for your movie catalog: Note this according to IMDB")
+                rating = newIn
+                currentData = dh.getMoviesForRatings(currentData, rating)
 
-
+    numberOfMoviesCurrently = len(currentData)
+    again = input("Keep filtering this list? (Y/N): ").strip().lower()
+        if again != 'y':
+            break
+    
 if __name__ == '__main__':
     interactiveCLI()
