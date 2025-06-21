8c5fe524f341d95b7bd743d40ffc6d0f7f9d009c
mcheema88 <marley.cheema1@ucalgary.ca>
2025-06-21 14:57:15 -0600

    Just developing logic for release date input -> a little confusing as we want to have options baked within -> so may be tricky if I make one function or an adaptable function. But progress is coming great

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import dataHandler as dh
 
 def interactiveCLI():
     data = dh.constructData()
+    currentData = data
 
     print("Hello! Welcome to The Movie Catalog. You wil have the ability to get a list of movies that match your specifications. \n" \
             "You can will have the option to sorts our list of over 200 000 movies by genre, release dates, actors and ratings! You can continue" \
@@ -22,25 +23,39 @@ def interactiveCLI():
         
         match userIn:
             case 1:
-                newIn = input(f"You have selected option [{userIn}]: find movies by actor \n" \
-                            "Please enter the name of the actor you would like to find or enter 0 for the main menu: ")
+                newIn = input(f"You have selected option [{userIn}]: Get movies by genre \n" \
+                            "Please enter a genre that you like to filter your movie catalog by or enter 0 for the main menu: ")
                 if(newIn == "0"):            
                     continue
-
-                else:
-                    print(dh.findMoviesByActor(data, newIn))
-                    print(dh.getActorStats(data, newIn))
                 
+                else: 
+                genre = newIn
+                currentData = dh.getMoviesForGenre(currentData, genre)
 
             case 2:
-                newIn = input(f"You have selected option [{userIn}]: find actors by movie \n" \
-                            "Please enter the name of the movie you would like to see data on, or enter 0 for the main menu: ")
-                if(newIn == "0"):
-                    continue
+                newIn = input(f"You have selected option [{userIn}]: Get movies by release date. \n" \
+                            "You now have three options, if you would like to filter for movies after a certain year enter : 1 \n
+                            "If you like to filter for movie before a certain year enter : 2 \n"
+                            "If you would like to filter for movie between two years enter : 3 \n")
+                #optionYears = newIn
 
-                else:
-                    print(userIn)
-                    print(dh.findActorMovies(data, newIn))
+                match newIn:
+                    case 1: 
+                        newNewIn = input(f"You have selected option [{newIn}]: Get movies after a certain year. \n" \
+                            "Please enter the year that you like to only see movies released after that date (not including)")
+                        year1 = newNewIn
+                        year2 = 2025
+                        
+                        currentData = dh.getMoviesForReleaseDate(currentData, year1, year2)
+
+                    case 2: 
+                        newNewIn = input(f"You have selected option [{newIn}]: Get movies before a certain year. \n" \
+                            "Please enter the year that you like to only see movies released after that date (not including)")
+                        year1 = newNewIn
+                        year2 = 0
+                        
+                        currentData = dh.getMoviesForReleaseDate(currentData, optionYears, year1, year2)
+
 
 
 
