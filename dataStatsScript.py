9dffce631b75f7be8e0987dc04e03622e3cba98f
mcheema88 <marley.cheema1@ucalgary.ca>
2025-06-23 13:13:42 -0600

    Okay now time for some intense debugging -> fixing column names with the new data -> making sure everything runs nicely ect.

--- dataStatsScript.py
+++ dataStatsScript.py
@@ -1,5 +1,16 @@
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import dataHandler as dH
+import numpy as np
 
-data 
+data = dH.construct_data()
+
+dH.averageRatingOfMoviesByYear(data)
+
+dH.averageRatingsRatingOfMoviesByYearAndGenre(data)
+
+print(dH.topActorsByRating(data))
+print(dH.topActressesByRating(data))
+
+dH.moviesByGenre(data)
+dH.votesVsRating(data)
